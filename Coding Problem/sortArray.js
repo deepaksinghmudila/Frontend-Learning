2e43362d49d4f8cfc9dcf5e94c588aa217bd2d87
deepaksinghmudila <deepaksinghmudila21@gmail.com>
2021-02-27 21:33:53 +0530

    swapping approach

--- Coding Problem/sortArray.js
+++ Coding Problem/sortArray.js
@@ -3,7 +3,8 @@
 /*  Solution:  */
 
 function sortArray(arr){
-  
+
+/******  for Counting the frequency of all 0's 1's and 2's in the array ********/
   let zero,one,two;
   for( let i=0; i<arr.length; i++  ){
       if( arr[i]==0 )
@@ -31,4 +32,34 @@ sortArray(arr);
 
 
 
- 
+/*****************************************************************************************************/ 
+/******* The following approach is swapping one. It is variant of Dutch Flag Problem  *****************/
+
+
+function sortArray(arr) {
+  
+  let low = 0; let mid = 0; let high = arr.length - 1;
+
+  while (mid <= high) { 
+        switch (a[mid]) { 
+  
+        // If the element is 0 
+        case 0: 
+            swap(a[low++], a[mid++]); 
+            break; 
+  
+        // If the element is 1 . 
+        case 1: 
+            mid++; 
+            break; 
+  
+        // If the element is 2 
+        case 2: 
+            swap(a[mid], a[high--]); 
+            break; 
+        } 
+    }   
+}
+
+
+
