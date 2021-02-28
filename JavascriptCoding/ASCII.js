d64133a91eb037b68095ac26c568f504c96b0b5a
deepaksinghmudila <deepaksinghmudila21@gmail.com>
2021-02-28 12:57:39 +0530

    ASCII

---
+++ JavascriptCoding/ASCII.js
@@ -0,0 +1,15 @@
+var string = "Some string";
+
+for (var i = 0; i < string.length; i++) {
+  console.log(string.charCodeAt(i));
+}
+
+//var string = "1928";
+let num = 108;
+//let string = ''+num;
+let  string  = num.toString(); 
+for (var i = 0; i < string.length; i++) {
+  console.log(string.charCodeAt(i));
+}
+
+ console.log("abcd".charCodeAt(0));
