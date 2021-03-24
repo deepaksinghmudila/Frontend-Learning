5aa7a5bdcfc988f733eda73729d77adabd458e30
deepaksinghmudila <deepaksinghmudila21@gmail.com>
2021-03-24 10:09:54 +0530

    commit

---
+++ Namaste Javascript/blockandshadowing.js
@@ -0,0 +1,12 @@
+{
+  var x = 10;
+  var y = 100;
+  var z = 1000;
+  console.log(x);
+  console.log(y);
+  console.log(z);
+}
+
+console.log(x);
+console.log(y);
+console.log(z);
---
+++ Namaste Javascript/letandconst.js
@@ -0,0 +1,5 @@
+console.log(X);
+let a = 10;
+var b = 100;
+
+
