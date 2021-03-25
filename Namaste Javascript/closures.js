53c68bb007b1e1202491f4ac40f015ff074efc75
deepaksinghmudila <deepaksinghmudila21@gmail.com>
2021-03-25 11:51:50 +0530

    setTimeout

--- Namaste Javascript/closures.js
+++ Namaste Javascript/closures.js
@@ -4,9 +4,12 @@ function x() {
     function y() {
         console.log(a);
     }
+    a = 100;
     return y;
 }
 
 var z = x();
 //console.log(z());
+
+// function forming closures only returns the reference of the variables not the value of them.
 z();
